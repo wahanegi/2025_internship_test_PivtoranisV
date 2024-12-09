d5231663430f96395429495bf6783afa680f7b7e
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-09 15:22:58 +0200

    Add tdd rspec tests for password validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,16 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:user_name).is_at_most(15) }
     it { should allow_value('valid_user123').for(:user_name) }
     it { should_not allow_value('invalid.user!').for(:user_name).with_message('only allows letters, numbers, and underscores') }
+    it { should allow_value('!abc123').for(:password) }
+    it { should_not allow_value('abc').for(:password) }
+    it { should_not allow_value('abcdefg').for(:password) }
+    it 'does not allow passwords with repeated characters more than twice' do
+      user = build(:user,
+               password: '!aaabcd',
+               password_confirmation: '!aaabcd',
+               email: 'test@example.com',
+               user_name: 'username')
+      expect(user).not_to be_valid
+    end
   end
 end
