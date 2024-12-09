a728ba0a2d894777c6fcc8007cdbb3630bed4074
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-10 00:02:06 +0200

    Add letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   config.secret_key_base = ENV["DEVISE_SECRET_KEY"]
+
+  # Set email delivery method
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
