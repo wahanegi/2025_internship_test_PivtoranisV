265b7318a1ffbb023de21f1e113d16efeb783540
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-09 15:53:39 +0200

    Add User validation for strong password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,14 @@ class User < ApplicationRecord
   validates :user_name, uniqueness: true
   validates :user_name, length: { maximum: 15 }
   validates :user_name, format: {
-  with: /\A[a-zA-Z0-9_]+\z/,
-  message: "only allows letters, numbers, and underscores"
+    with: /\A[a-zA-Z0-9_]+\z/,
+    message: "only allows letters, numbers, and underscores"
 }
+  validates :password, length: { minimum: 6 }
+  validates :password, format: {
+    with: /\A(?=.*[^\w\s])[^\s]*\z/,
+    message: "must contain at least one symbol" }
+  validates :password, format: {
+    with: /\A(?!.*(.)\1\1).*\z/,
+    message: "cannot repeat the same character more than twice" }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     user_name { Faker::Internet.username(specifier: 1..15) }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password { '@password' }
+    password_confirmation { '@password' }
   end
 end
