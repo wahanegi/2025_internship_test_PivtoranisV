eb876d7348665b12843a7ec9940ae8fc8733f0c2
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-09 20:02:55 +0200

    Fixed FactoryBot for user_name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
-    user_name { Faker::Internet.username(specifier: 1..15) }
+    user_name { Faker::Internet.unique.username(specifier: 1..15).gsub(/[^a-zA-Z0-9_]/, '') }
     password { '@password' }
     password_confirmation { '@password' }
   end
