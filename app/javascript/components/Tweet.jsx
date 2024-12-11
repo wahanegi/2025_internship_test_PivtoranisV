41c59be286971da3699389f7e53df6e3980fdf24
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-11 16:21:30 +0200

    Fixed date formatting

--- app/javascript/components/Tweet.jsx
+++ app/javascript/components/Tweet.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import {
+  formatDistanceToNow,
+  parseISO,
+  differenceInDays,
+  format,
+} from 'date-fns';
 
 const Tweet = ({ content, author, date }) => {
-  const time = new Date(date).toLocaleDateString('en-US', {
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
-  });
+  const parsedDate = parseISO(date);
+  const daysDifference = differenceInDays(new Date(), parsedDate);
+
+  const displayTime =
+    daysDifference <= 1
+      ? formatDistanceToNow(parsedDate, { addSuffix: true })
+      : format(parsedDate, 'MMM dd, yy');
 
   return (
     <section>
       <div className="py-2 border-bottom border-secondary">
         <p>{author}</p>
-        <p>{time}</p>
+        <p>{displayTime}</p>
         <p>{content}</p>
       </div>
     </section>
