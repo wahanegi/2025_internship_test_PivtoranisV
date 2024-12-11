fc29a533d3d04d70cf3badc0afe6e96fa6123c06
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-11 10:39:17 +0200

    Add React routing

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="container mt-5">
-      <h1 className="text-success">Hello Twitter</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ app/javascript/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-success">Hello Twitter</h1>
+    </div>
+  );
+};
+
+export default Home;
