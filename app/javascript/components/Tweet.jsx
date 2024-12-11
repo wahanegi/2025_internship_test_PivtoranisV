39e61107443d51fc8c235ffb27f3a12f62a1cfa2
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-11 15:49:44 +0200

    Fixed date format

--- app/javascript/components/Tweet.jsx
+++ app/javascript/components/Tweet.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Tweet = ({ content, author, date }) => {
+  const time = new Date(date).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
   return (
     <section>
       <div className="py-2 border-bottom border-secondary">
         <p>{author}</p>
-        <p>{date}</p>
+        <p>{time}</p>
         <p>{content}</p>
       </div>
     </section>
