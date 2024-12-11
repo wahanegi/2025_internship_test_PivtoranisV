120e08ea07b40d89b4e7f5314137253ba8cad814
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-11 10:31:21 +0200

    Add fallback routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static#home"
   get "up" => "rails/health#show", as: :rails_health_check
+  get "*path", to: "static#home", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
