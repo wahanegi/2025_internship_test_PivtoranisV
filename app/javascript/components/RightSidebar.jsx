86d55893208b80286ea4dbea4ac3d862d5e870c7
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-12 11:36:49 +0200

    Fixed issue with sign in only after second click

--- app/javascript/components/RightSidebar.jsx
+++ app/javascript/components/RightSidebar.jsx
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Col } from 'react-bootstrap';
 import Suggestions from './Suggestions';
 import UserAuthentication from './UserAuthentication';
 
-const RightSideBar = () => {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      const response = await fetch('http://localhost:3000/api/v1/users');
-      const tweetData = await response.json();
-      setCurrentUser(tweetData.data);
-    };
-    fetchCurrentUser();
-  }, []);
-
-  console.log(currentUser);
+const RightSideBar = ({ user }) => {
   return (
     <Col xs={4} className="h-100">
-      {currentUser ? <Suggestions /> : <UserAuthentication />}
+      {user ? <Suggestions /> : <UserAuthentication />}
     </Col>
   );
 };
--- app/javascript/components/pages/Home.jsx
+++ app/javascript/components/pages/Home.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import MainContent from '../MainContent';
 import LeftSideBar from '../LeftSideBar';
 import RightSideBar from '../RightSideBar';
 
 const Home = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/v1/users', {
+          headers: {
+            Accept: 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          if (response.status === 401) {
+            setCurrentUser(null);
+          } else {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+        } else {
+          const userData = await response.json();
+          setCurrentUser(userData.data);
+        }
+      } catch (error) {
+        console.error('Failed to fetch user:', error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
+  console.log(currentUser);
   return (
     <Container fluid className="my-2">
       <Row className="gap-2">
         <LeftSideBar />
         <MainContent />
-        <RightSideBar />
+        <RightSideBar user={currentUser} />
       </Row>
     </Container>
   );
