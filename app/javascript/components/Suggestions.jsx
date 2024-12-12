89b7e216488897feb56f8b4225a5e8dbe86afa95
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-13 00:05:54 +0200

    Update RightSidebar style

--- app/javascript/components/Suggestions.jsx
+++ app/javascript/components/Suggestions.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Button, Card, Col, Form, Row } from 'react-bootstrap';
+import { Card, Form, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
 
 const Suggestions = () => {
   return (
     <>
-      <Form className="my-2">
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2 bg-secondary border-0"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit" variant="primary" className="rounded-pill">
-              Search
-            </Button>
-          </Col>
-        </Row>
+      <Form className="my-2 me-5">
+        <InputGroup className="bg-secondary rounded">
+          <InputGroup.Text className="bg-secondary border-0">
+            <FaSearch className="text-dark" />
+          </InputGroup.Text>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="mr-sm-2 bg-secondary border-0 search"
+          />
+        </InputGroup>
       </Form>
-      <Card className="card-black border border-secondary me-5">
+      <Card className="card-black border border-secondary me-5 mt-5">
         <Card.Body>
           <Card.Title className="my-2">What’s happening</Card.Title>
           <Card.Subtitle className="text-secondary mt-4 small">
@@ -49,7 +46,7 @@ const Suggestions = () => {
           </Card.Text>
         </Card.Body>
       </Card>
-      <Card className="card-black border border-secondary mt-5 me-5">
+      <Card className="card-black border border-secondary mt-2 me-5">
         <Card.Body>
           <Card.Title className="my-2">Who to follow</Card.Title>
           <Card.Subtitle className="text-secondary mt-4 small">
@@ -66,6 +63,12 @@ const Suggestions = () => {
           <Card.Text>Follow</Card.Text>
         </Card.Body>
       </Card>
+      <p className="text-secondary small mt-5">
+        Terms of Service · Privacy Policy · Cookie Policy · Accessibility
+      </p>
+      <p className="text-secondary small text-center me-5">
+        &copy; 2024 PivtoranisV
+      </p>
     </>
   );
 };
