803fa05805c278b6bb5071a371e2406e722604ce
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-13 16:16:41 +0200

    Add production URL to ENV variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "https://internship-project-pivtoranisv-286306d1a130.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: ENV["DOMAIN_URL"] }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
