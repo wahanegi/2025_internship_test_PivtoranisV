14611b1a0f660c6d92f40d3e6cbb1d5cf126be7c
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-16 23:47:11 +0200

    Updated API URLs in React components

--- app/javascript/components/pages/Home.jsx
+++ app/javascript/components/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/v1/users', {
+        const response = await fetch('/api/v1/users', {
           headers: {
             Accept: 'application/json',
           },
