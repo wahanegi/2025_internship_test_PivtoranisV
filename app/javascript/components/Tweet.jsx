9c5f57593ba78218c48050a7c277c8669499cebd
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-20 00:36:13 +0200

    Refactor time calculation to use utility function for it

--- app/javascript/components/Tweet.jsx
+++ app/javascript/components/Tweet.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  formatDistanceToNow,
-  parseISO,
-  differenceInDays,
-  format,
-} from 'date-fns';
 import TweetActionLinks from './TweetActionLinks';
 import { useNavigate } from 'react-router-dom';
+import { getDisplayTime } from '../utils';
 
 const Tweet = ({
   content,
@@ -24,13 +19,7 @@ const Tweet = ({
 }) => {
   const navigate = useNavigate();
 
-  const parsedDate = parseISO(date);
-  const daysDifference = differenceInDays(new Date(), parsedDate);
-
-  const displayTime =
-    daysDifference <= 1
-      ? formatDistanceToNow(parsedDate, { addSuffix: true })
-      : format(parsedDate, 'MMM dd, yy');
+  const displayTime = getDisplayTime(date);
 
   const handleNavigate = () => {
     if (!sentFromDetails) {
--- app/javascript/utils.js
+++ app/javascript/utils.js
@@ -1,3 +1,18 @@
+import {
+  formatDistanceToNow,
+  parseISO,
+  differenceInDays,
+  format,
+} from 'date-fns';
+
+export const getDisplayTime = (date) => {
+  const daysDifference = differenceInDays(new Date(), parseISO(date));
+
+  return daysDifference <= 1
+    ? formatDistanceToNow(date, { addSuffix: true })
+    : format(date, 'MMM dd, yy');
+};
+
 export const getCSRFToken = () => {
   const meta = document.querySelector('meta[name="csrf-token"]');
   return meta ? meta.getAttribute('content') : null;
